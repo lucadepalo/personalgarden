ca25aea1f01757a9f781c8ddc9d6be9187c8d2af
Luca Depalo <l.depalo8@studenti.uniba.it>
2023-03-02 17:07:47 +0100

    First commit

---
+++ app/src/main/java/com/lucadepalo/smartirrigator/URLs.java
@@ -0,0 +1,10 @@
+package com.lucadepalo.smartirrigator;
+
+public class URLs {
+
+    private static final String ROOT_URL = "http://172.29.22.239/Api.php?apicall=";
+
+    public static final String URL_REGISTER = ROOT_URL + "signup";
+    public static final String URL_LOGIN = ROOT_URL + "login";
+
+}
---
+++ app/src/main/java/com/lucadepalo/smartirrigator/User.java
@@ -0,0 +1,40 @@
+package com.lucadepalo.smartirrigator;
+
+public class User {
+
+    private int id;
+    private String username, email, nome, cognome;
+
+    public User(int id, String username, String email, String nome, String cognome) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public String getCognome() {
+
+        return cognome;
+    }
+}
