040a3d55b3f8a4fbf44aa46d448f4a3c40cfb424
Luca Depalo <l.depalo8@studenti.uniba.it>
2023-03-13 14:26:00 +0100

    Added candidate for dns resolving and link handling

--- app/src/main/java/com/lucadepalo/smartirrigator/URLs.java
+++ app/src/main/java/com/lucadepalo/smartirrigator/URLs.java
@@ -1,12 +1,35 @@
 package com.lucadepalo.smartirrigator;
 
+/*
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+ */
 public class URLs {
 
-    //private static final String ROOT_URL = "http://lucadepalo.dynamic-dns.net/Api.php?apicall=";
-    private static final String ROOT_URL = "http://172.29.35.34/Api.php?apicall=";
+    /*
+    private static final String url = "lucadepalo.dynamic-dns.net";
 
+    private static final InetAddress address;
+
+    static {
+        try {
+            address = InetAddress.getByName(url);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final String ip = address.getHostAddress();
+
+    private static final String ROOT_URL = "http://" + ip + "/Api.php?apicall=";
+     */
+    private static final String ROOT_URL = "http://172.29.35.34/Api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
     public static final String URL_LOGIN = ROOT_URL + "login";
 
+    /*
+    public URLs() throws UnknownHostException {
+    }
+    */
 }
