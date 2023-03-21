fc9a78c043b7a2e62305280018854670e2506171
Luca Depalo <l.depalo8@studenti.uniba.it>
2023-03-22 00:07:26 +0100

    Added checks on QR codes so that the activity will successfully terminate if and only if the user has scanned both valid qr codes in any order

--- app/src/main/java/com/lucadepalo/smartirrigator/QRcodeActivity.java
+++ app/src/main/java/com/lucadepalo/smartirrigator/QRcodeActivity.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
 public class QRcodeActivity extends AppCompatActivity implements View.OnClickListener{
     Button scanBtn;
     TextView messageText;
-    String qrSUT = "", qrAIRR = "";
+    String temp = "", qrSUT = "", qrAIRR = "", prefS = "SUT", prefA = "AIRR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,32 +65,27 @@ public class QRcodeActivity extends AppCompatActivity implements View.OnClickLis
                 // if the intentResult is not null we'll set
                 // the content and format of scan message
                 messageText.setText(intentResult.getContents());
-
-                if (qrSUT.isEmpty()) {
-                    qrSUT = messageText.getText().toString().trim();
-
-
-                } else if (qrAIRR.isEmpty()) {
-                    qrAIRR = messageText.getText().toString().trim();
-                    if(!qrSUT.isEmpty()&&!qrAIRR.isEmpty()){
-                        registerCodes();
-                    }
-
+                temp = messageText.getText().toString().trim();
+                // if the SUT qr code hasn't already been scanned AND if the code scanned starts with SUT
+                if (qrSUT.isEmpty() && temp.startsWith(prefS)) {
+                    qrSUT = temp;
+                    // if the AIRR qr code hasn't already been scanned AND if the code scanned starts with AIRR
+                } else if (qrAIRR.isEmpty() && temp.startsWith(prefA)) {
+                    qrAIRR = temp;
                 }
+                if(!qrSUT.isEmpty()&&!qrAIRR.isEmpty()){
+                    registerCodes();
+                    //this calls the next activity that needs to run after everything's done in this
+                    //startActivity(new Intent(getApplicationContext(), NextActivity.class));
+                    finish();
+                } /*else {
+                    Toast.makeText(getBaseContext(), "Ripetere le scansioni!", Toast.LENGTH_LONG).show();
+                    qrSUT = ""; qrAIRR = ""; temp = "";
+                }*/
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
-
-        /*if(!qrSUT.isEmpty()&&!qrAIRR.isEmpty()){
-            findViewById(R.id.scanBtn).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    registerCodes();
-                }
-            });
-            //startActivity(new Intent(getApplicationContext(), NextActivity.class));
-        }*/
     }
 
     private void registerCodes(){
