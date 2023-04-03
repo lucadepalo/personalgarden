6a3a9bf3c4b5242f6c3a7b1608d9333cd432bd46
Luca Depalo <l.depalo8@studenti.uniba.it>
2023-04-03 15:11:26 +0200

    revamped style

--- app/src/main/java/com/lucadepalo/personalgarden/URLs.java
+++ app/src/main/java/com/lucadepalo/personalgarden/URLs.java
@@ -1,38 +1,15 @@
 package com.lucadepalo.personalgarden;
 
-/*
-import java.net.InetAddress;
-import java.net.UnknownHostException;
- */
 public class URLs {
 
-    /*
-    private static final String url = "lucadepalo.dynamic-dns.net";
+    private static final String ROOT_URL = "http://172.29.7.21/Api.php?apicall=";
 
-    private static final InetAddress address;
-
-    static {
-        try {
-            address = InetAddress.getByName(url);
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static final String ip = address.getHostAddress();
-
-    private static final String ROOT_URL = "http://" + ip + "/Api.php?apicall=";
-     */
-    private static final String ROOT_URL = "http://172.29.56.203/Api.php?apicall=";
     //private static final String ROOT_URL = "http://192.168.1.149/Api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
+
     public static final String URL_LOGIN = ROOT_URL + "login";
 
     public static final String URL_QRCODE = ROOT_URL + "qrcode";
 
-    /*
-    public URLs() throws UnknownHostException {
-    }
-    */
 }
