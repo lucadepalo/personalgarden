38c7fadde010e5d26536b598660db6ce0b24d2f9
Luca Depalo <l.depalo8@studenti.uniba.it>
2023-08-02 02:31:09 +0200

    new animation in qr scan and minor improvements

--- app/src/main/java/com/lucadepalo/personalgarden/QRcodeActivity.java
+++ app/src/main/java/com/lucadepalo/personalgarden/QRcodeActivity.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -18,12 +21,16 @@ import java.util.HashMap;
 public class QRcodeActivity extends AppCompatActivity implements View.OnClickListener{
     Button scanBtn;
     TextView messageText;
+    ImageView gifImageView;
     String temp = "", qrSUT = "", qrAIRR = "", prefS = "SUT", prefA = "AIRR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+
+
 
         if (SharedPrefManager.getInstance(this).isCoupled()) {
             finish();
@@ -35,10 +42,12 @@ public class QRcodeActivity extends AppCompatActivity implements View.OnClickLis
         // the button and textviews
         scanBtn = findViewById(R.id.scanBtn);
         messageText = findViewById(R.id.textContent);
+        gifImageView = findViewById(R.id.gifImageView);
         //messageFormat = findViewById(R.id.textFormat);
 
         // adding listener to the button
         scanBtn.setOnClickListener(this);
+        Glide.with(this).load(R.drawable.scan).into(gifImageView);
     }
 
     @Override
--- app/src/main/java/com/lucadepalo/personalgarden/URLs.java
+++ app/src/main/java/com/lucadepalo/personalgarden/URLs.java
@@ -4,8 +4,8 @@ public class URLs {
     //private static final String ROOT_URL = "http://172.29.29.89/Api.php?apicall=";
     //private static final String ROOT_URL = "http://192.168.126.43/Api.php?apicall=";
 
-    private static final String ROOT_URL = "http://lucadepalo.dynamic-dns.net/Api.php?apicall=";
-    //private static final String ROOT_URL = "http://192.168.1.149/Api.php?apicall=";
+    //private static final String ROOT_URL = "http://lucadepalo.dynamic-dns.net/Api.php?apicall=";
+    private static final String ROOT_URL = "http://192.168.1.149/Api.php?apicall=";
     public static final String URL_REGISTER = ROOT_URL + "signup";
     public static final String URL_LOGIN = ROOT_URL + "login";
     public static final String URL_QRCODE = ROOT_URL + "qrcode";
@@ -15,4 +15,16 @@ public class URLs {
     public static final String URL_DISPONE = ROOT_URL + "dispone";
     public static final String URL_ASSEGNATA = ROOT_URL + "assegnata";
 
+
+/*
+    private static final String ROOT_URL = "https://personalgarden.000webhostapp.com/";
+    public static final String URL_REGISTER = ROOT_URL + "signup.php";
+    public static final String URL_LOGIN = ROOT_URL + "login.php";
+    public static final String URL_QRCODE = ROOT_URL + "qrcode.php";
+    public static final String URL_CROPLIST = ROOT_URL + "croplist.php";
+    public static final String URL_SUGGEST = ROOT_URL + "suggest.php";
+    public static final String URL_IRRIGA = ROOT_URL + "irriga.php";
+    public static final String URL_DISPONE = ROOT_URL + "dispone.php";
+    public static final String URL_ASSEGNATA = ROOT_URL + "assegnata.php";
+*/
 }
